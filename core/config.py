0744b3cfc242da96dace8604c22f2a8f5ba00568
sakenzi <ermekulysaken8@gmail.com>
2025-11-12 12:43:09 +0500

    Added completed config

--- core/config.py
+++ core/config.py
@@ -13,4 +13,38 @@ class Settings(BaseSettings):
         case_sensitive=False,
     )
 
+    DB_HOST: str = "localhost"
+    DB_PORT: str = "5432"
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    TOKEN_SECRET_KEY: str
+    TOKEN_ALGORITHM: str = "HS256"
+    TOKEN_EXPIRE_MINUTES: int = 60 * 15
+
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_SERVER: str
+    MAIL_PORT: str
+    MAIL_FROM: str = ""
+
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
+    REDIS_DB: int = 0
+
+    DATETIME_FORMAT: str = "%d-%m-%Y &H:%M:%S"
+
+    @property
+    def DATABASE_URL_asyncpg(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
     
+    @property
+    def DATABASE_URL_psycopg2(self) -> str:
+        return f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+    @property
+    def REDIS_URL(self) -> str:
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_DB}"
+    
+settings = Settings()
